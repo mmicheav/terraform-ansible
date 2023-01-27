f5440dbbd1637135486ebdf167d0eb9a2839c9e0
mmicheav <34027239+mmicheav@users.noreply.github.com>
2023-01-26 23:48:44 -0300

    Update provider.tf
    remove client secret

--- AKS/provider.tf
+++ AKS/provider.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
     subscription_id = "4df13fce-d1c9-4c96-91d4-b9d6970008a0"
     client_id = "d8d738bc-ff51-44e2-a579-4ea49ea98792"
-    client_secret = "WKZ8Q~4fx4blRElhlh1hx2i4tu~dY3kvLqZFIc6d"
+    client_secret = "*********"
     tenant_id = "973803b3-1572-4f51-bad5-fabbbd3960d0"
     features {
     }
